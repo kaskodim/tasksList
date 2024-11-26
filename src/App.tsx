1ff5a27ca225f50dcf676f04c680a02e4ba13fa2
DmitriyGulevich <kaskodim@gmail.com>
2024-11-26 23:08:28 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="App">
+            Здесь скоро будет приложение списка дел ;)
+        </div>
+    );
+}
+
+export default App;
